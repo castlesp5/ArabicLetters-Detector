7effa9f3a5670e3a992aae5874522457e2cd0db9
Ismail Boujdad <iboujdad@c1r4p12.1337.ma>
2025-12-27 21:37:30 +0100

    B

---
+++ b.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+from PIL import Image
+
+IMGS = 32
+LR = 0.1
+EPOCHS = 1000
+
+X = []
+Y = []
+
+def load_images(folder, label):
+    for f in os.listdir(folder):
+        path = os.path.join(folder, f)
+        img = Image.open(path).convert("L")
+        img = img.resize((IMGS, IMGS))
+        arr = np.array(img).flatten() / 255
+        X.append(arr)
+        Y.append(label)
+
+load_images("B/", 1)
+load_images("A/", 0)
+
+
+X = np.array(X)
+Y = np.array(Y)
+
+print("Training Samples:", len(X))
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+np.random.seed(0)
+w = np.random.randn(IMGS * IMGS) * 0.01
+b = 0
+
+for epoch in range(EPOCHS):
+    z = np.dot(X,w) + b
+    yy = sigmoid(z)
+
+    dz = yy - Y
+    dW = np.dot(X.T, dz) / len(X)
+    db = np.mean(dz)
+
+    w -= LR * dW
+    b -= LR * db
+
+
+img = Image.open("char.png").convert("L")
+img = img.resize((IMGS, IMGS))
+x = np.array(img).flatten() / 255
+
+pr = sigmoid(np.dot(x,w) + b)
--- learn.py
+++ learn.py
@@ -1,59 +1,13 @@
-import os
-import numpy as np
-from PIL import Image
+import a
 
-IMGS = 32
-LR = 0.1
-EPOCHS = 1000
-
-X = []
-Y = []
-
-def load_images(folder, label):
-    for f in os.listdir(folder):
-        path = os.path.join(folder, f)
-        img = Image.open(path).convert("L")
-        img = img.resize((IMGS, IMGS))
-        arr = np.array(img).flatten() / 255
-        X.append(arr)
-        Y.append(label)
-
-load_images("A/", 1)
-load_images("Not_A/", 0)
-
-
-X = np.array(X)
-Y = np.array(Y)
-
-print("Training Samples:", len(X))
-
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-
-np.random.seed(0)
-w = np.random.randn(IMGS * IMGS) * 0.01
-b = 0
-
-for epoch in range(EPOCHS):
-    z = np.dot(X,w) + b
-    yy = sigmoid(z)
-
-    dz = yy - Y
-    dW = np.dot(X.T, dz) / len(X)
-    db = np.mean(dz)
-    
-    w -= LR * dW
-    b -= LR * db
-
-
-img = Image.open("char.png").convert("L")
-img = img.resize((IMGS, IMGS))
-x = np.array(img).flatten() / 255
-
-pr = sigmoid(np.dot(x, w) + b)
-print(pr * 100, "%")
-
-if pr > 0.6:
-    print("LETTER AAAA")
+if a.pr > 0.6:
+    print("Letter A")
 else :
-    print("MACHI LETTER AAAA")
+    print("Machi A")
+
+import b
+
+if b.pr > 0.6:
+    print("Letter B")
+else:
+    print("Machi B")
